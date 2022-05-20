ee3af4ff644c9aa0f8bc4031adc06f5aacca2e2d
saad khan <iam.saadk@gmail.com>
2022-05-21 00:04:26 +0530

    added img folder and setting up api file

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const base_URL = 'https://api.rawg.io/api/';
+
+const getCurrentMonth = () => {
+	const month = new Date().getMonth() + 1;
+	return month < 10 ? '0' + month : month;
+};
+
+const getCurrentDay = () => {
+	const day = new Date().getDay() + 1;
+	return day < 10 ? '0' + day : day;
+};
+
+const currentDay = getCurrentDay();
+const currentMonth = getCurrentMonth();
+const currentYear = new Date().getFullYear();
+
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYearDate = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYearDate = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+const popularGames = `games?key=${process.env.REACT_APP_API}&dates=${lastYearDate},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = `${base_URL}${popularGames}`;
